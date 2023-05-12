5c67f718bdee46c7df741e12432b073f56466e01
aston <olekston@gmail.com>
2023-05-12 12:31:13 +0200

    dam rade

--- src/test/java/com/company/FoodProductTest.java
+++ src/test/java/com/company/FoodProductTest.java
@@ -1,30 +1,49 @@
 package com.company;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
+import java.lang.reflect.Field;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FoodProductTest {
+
     @Test
-    void fromCsvTest(){
-        Path file = Path.of("C:\\Users\\Student\\Desktop\\Aleksadner Stoń\\testy\\src\\test\\resources\\buraki.csv");
-        FoodProduct test = FoodProduct.fromCsv(file);
-        assertEquals("Buraki - za 1 kg",test.getName());
-    }
-    @Test
-    void fromCsvTest2(){
-        Path file = Path.of("C:\\Users\\Student\\Desktop\\Aleksadner Stoń\\testy\\src\\test\\resources\\buraki.csv");
-        FoodProduct test = FoodProduct.fromCsv(file);
-        assertEquals(1.53,test.getPrice(2010,1,"DOLNOŚLĄSKIE"));
+    void fromCsvName() {
+        FoodProduct product = FoodProduct.fromCsv(Path.of("test/resources/testfoodproduct.csv"));
+        assertEquals("produkt testowy", product.getName());
     }
 
     @Test
-    void fromCsvTest3(){
-        Path file = Path.of("C:\\Users\\Student\\Desktop\\Aleksadner Stoń\\testy\\src\\test\\resources\\buraki.csv");
-        FoodProduct test = FoodProduct.fromCsv(file);
-        assertEquals(1.53,test.getPrice(2010,1,"DOLNOŚLĄSKIE"));
+    void fromCsvPrice() throws NoSuchFieldException, IllegalAccessException {
+        FoodProduct product = FoodProduct.fromCsv(Path.of("test/resources/testfoodproduct.csv"));
+
+        // dostęp do prywatnego pola - zła praktyka
+        Field pricesField = FoodProduct.class.getDeclaredField("prices");
+        pricesField.setAccessible(true);
+        Map<String, Double[]> prices = (Map<String, Double[]>) pricesField.get(product);
+
+        assertEquals(6.0, prices.get("yyy")[2]);
     }
 
+    @Test
+    void fromCsvPrice2() {
+        FoodProduct product = FoodProduct.fromCsv(Path.of("test/resources/testfoodproduct.csv"));
+        FoodProductExposed exposedProduct = new FoodProductExposed(product);
+
+        assertEquals(6.0, exposedProduct.getPrices().get("yyy")[2]);
+    }
+
+    @Test
+    void fromCsvPrice3() {
+        FoodProduct product = FoodProduct.fromCsv(Path.of("test/resources/testfoodproduct.csv"));
+
+        assertEquals(6.0, product.getPrice(2010, 3, "yyy"));
+    }
 }
--- src/test/java/com/company/ProductTest.java
+++ src/test/java/com/company/ProductTest.java
@@ -1,53 +1,134 @@
 package com.company;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ProductTest {
+
     @Test
-    void priceIndexTestResult(){
-        assertEquals(2,Product.priceIndex(2010,3));
+    void priceIndex() {
+        int year = 2011;
+        int month = 5;
+        int expectedIndex = 16;
+
+        assertEquals(expectedIndex, Product.priceIndex(year, month));
     }
+
     @Test
-    void priceIndexTestExeption(){
-        assertThrows(IndexOutOfBoundsException.class,()->{
-           Product.priceIndex(2005,5);
-        });
+    void priceIndexThrowsException() {
+        int year = 2000;
+        int month = 1;
+
+         assertThrows(IndexOutOfBoundsException.class, () -> Product.priceIndex(year, month));
     }
-    private static Stream<Arguments> getMonthYearIndexes() {
+
+
+    private static Stream<Arguments> monthYearIndex() {
         return Stream.of(
-                Arguments.of(2010,1,0),
-                Arguments.of(2010,11, 10),
-                Arguments.of(2011,11, 22),
-                Arguments.of(2020,5,124)
+                arguments(2012, 12, 35),
+                arguments(2022, 3, 146),
+                arguments(2020, 5, 124)
         );
     }
+
     @ParameterizedTest
-    @MethodSource("getMonthYearIndexes")
-    void priceIndexParameterized(int year,int month, int expected){
-        assertEquals(expected,Product.priceIndex(year,month));
+    @MethodSource("monthYearIndex")
+    void priceIndex(int year, int month, int expectedIndex) {
+        assertEquals(expectedIndex, Product.priceIndex(year, month));
     }
+
     @ParameterizedTest
-    @CsvFileSource(resources ="/test.csv",numLinesToSkip = 1, delimiter = ';')
-    void priceIndexFromCsv(int year,int month, int expected){
-        assertEquals(expected,Product.priceIndex(year,month));
+    @CsvFileSource(files = "test/resources/months.csv")
+    void priceIndex(String date, int year, int month, int expectedIndex) {
+        assertEquals(expectedIndex, Product.priceIndex(year, month));
     }
+
     @Test
-    void priceIndexMultiple(){
-        int expected=0;
-        for(int year= 2010;year<=2020;year++){
-            for(int month : IntStream.rangeClosed(1,12).toArray()){
-                assertEquals(expected,Product.priceIndex(year,month));
-                expected++;
+    void priceIndexMultiple() {
+        int expectedIndex = 0;
+        for(int year : IntStream.rangeClosed(2001, 2020).toArray()) {
+            for(int month : IntStream.rangeClosed(1, 12).toArray()) {
+                assumeTrue(year>2010);
+                assertEquals(expectedIndex, Product.priceIndex(year, month));
+                expectedIndex++;
             }
+        }
+    }
+
+    @Nested
+    class GetProductTest {
+        static Path tmpDir;
+        private static void createFileStub(Path dir, String name) throws IOException {
+            File file = File.createTempFile("product", ".csv", dir.toFile());//new (String.valueOf(dir.resolve(filename)));
+            // file.deleteOnExit();
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(name);
+            printWriter.println(); // linia z nagłówkiem
+            printWriter.println(); // linia z cenami
+            printWriter.close();
+
+        }
+
+        @BeforeAll
+        static void populateProducts() throws IOException {
+            tmpDir = Files.createTempDirectory("products");
+            //tmpDir.toFile().deleteOnExit(); // nie działa, kiedy są pliki, nawet auto-usuwane
+
+            System.out.println(tmpDir);
+
+            createFileStub(tmpDir, "Aaa1");
+            createFileStub(tmpDir, "Aaa2");
+            createFileStub(tmpDir, "Aaa3x");
+            createFileStub(tmpDir, "Bbb");
+
+            Product.addProducts(FoodProduct::fromCsv, tmpDir);
+        }
+
+        @AfterAll
+        static void cleanup() {
+            for(File file : tmpDir.toFile().listFiles()) {
+                System.out.println(file + " deleted.");
+                file.delete();
+            };
+            tmpDir.toFile().delete();
+        }
+
+        @Test
+        void getProductTest() throws AmbigiousProductException {
+            Product product = Product.getProduct("B");
+            assertEquals("Bbb", product.getName());
+        }
+
+        @TestFactory
+        Stream<DynamicTest> dynamicGetProductTest() {
+            List<String> input = Arrays.asList("Aaa1", "Aaa2", "Aaa3", "B");
+            List<String> output = Arrays.asList("Aaa1", "Aaa2", "Aaa3x", "Bbb");
+
+            return input.stream()
+                    .map(prefix -> DynamicTest.dynamicTest(
+                            "Test: "+prefix,
+                            ()-> {
+                                int id = input.indexOf(prefix);
+                                assertEquals(output.get(id), Product.getProduct(prefix).getName());
+                            }
+                    ));
         }
     }
 }
